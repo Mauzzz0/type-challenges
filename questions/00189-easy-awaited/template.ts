6b502f7c1cfda64488e5c9b8085513e16452ea31
Mauzzz0 <48735421+Mauzzz0@users.noreply.github.com>
2023-09-28 23:51:18 +0300

    189 easy awaited

--- questions/00189-easy-awaited/template.ts
+++ questions/00189-easy-awaited/template.ts
@@ -1,1 +1,1 @@
-type MyAwaited<T> = T extends PromiseLike<infer U> ? U extends Promise<unknown> ? MyAwaited<U> : U : never;
+type MyAwaited<T extends PromiseLike<any>> = T extends PromiseLike<infer U> ? U extends Promise<unknown> ? MyAwaited<U> : U : never;
